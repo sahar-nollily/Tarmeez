a03320552c66a3e96b9581bd8cd8877822ab86a9
lamiaewais <lamia5980@gmail.com>
2021-04-25 01:39:41 +0300

    delete unused code

--- app/src/main/java/com/tarmeez/game/MainActivity.kt
+++ app/src/main/java/com/tarmeez/game/MainActivity.kt
@@ -9,32 +9,10 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    lateinit var fab: FloatingActionButton
-    lateinit var bottomNavigationView: BottomNavigationView
-    lateinit var bottomAppBar: BottomAppBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         supportActionBar?.hide()
-        /*
-        fab = findViewById(R.id.fab)
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
-        bottomAppBar = findViewById(R.id.bottomAppBar)
-        val navController: NavController =
-            Navigation.findNavController(this, R.id.fragment_container)
-        bottomNavigationView.background = null
-
-        fab.setOnClickListener {
-            navController.navigate(R.id.homeFragment)
-        }
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        findNavController(R.id.fragment_container)
-
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
-        findNavController(R.id.fragment_container)
-
-         */
     }
 }
